c900cd5abcb8ab9d19f0439d8833a7d1548aa9c0
dobefore <1432338032@qq.com>
2022-11-12 11:59:03 +0800

    version 3.39.0

--- arm64.sh
+++ arm64.sh
@@ -1,10 +1,10 @@
 # compile sqlite
 
-wget --no-check-certificate https://www.sqlite.org/2022/sqlite-autoconf-3390400.tar.gz
-tar -zxvf sqlite-autoconf-3390400.tar.gz -C .
+wget --no-check-certificate https://www.sqlite.org/2022/sqlite-autoconf-3390000.tar.gz
+tar -zxvf sqlite-autoconf-3390000.tar.gz -C .
 
 mkdir -p $HOME/sql
-cd sqlite-autoconf-3390400
+cd sqlite-autoconf-3390000
 export PATH="$HOME/aarch64-linux-musl-cross/bin:$PATH"
 ./configure CC=aarch64-linux-musl-gcc  --host=aarch64 --prefix=$HOME/sql
 make && make install
